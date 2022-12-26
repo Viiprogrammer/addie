8e51ab80b54c5071110efbc7bcf139774605b0bd
Vadimka Komissarov <admin@vkom.cc>
2022-12-26 23:53:09 +0000

    fix SCC-U1000

--- app/app.go
+++ app/app.go
@@ -5,7 +5,6 @@ import (
 	"crypto/md5"
 	"encoding/base64"
 	"errors"
-	"fmt"
 	"net/url"
 	"os"
 	"os/signal"
@@ -126,25 +125,25 @@ func (*App) checkErrorsBeforeClosing(errs chan error) {
 	}
 }
 
-func (*App) defaultHandler(ctx *fasthttp.RequestCtx) {
-	fmt.Fprintf(ctx, "Hello, world!\n\n")
+// func (*App) defaultHandler(ctx *fasthttp.RequestCtx) {
+// 	fmt.Fprintf(ctx, "Hello, world!\n\n")
 
-	fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
-	fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
-	fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
-	fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
-	fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
-	fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
-	fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
-	fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
-	fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
-	fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
+// 	fmt.Fprintf(ctx, "Request method is %q\n", ctx.Method())
+// 	fmt.Fprintf(ctx, "RequestURI is %q\n", ctx.RequestURI())
+// 	fmt.Fprintf(ctx, "Requested path is %q\n", ctx.Path())
+// 	fmt.Fprintf(ctx, "Host is %q\n", ctx.Host())
+// 	fmt.Fprintf(ctx, "Query string is %q\n", ctx.QueryArgs())
+// 	fmt.Fprintf(ctx, "User-Agent is %q\n", ctx.UserAgent())
+// 	fmt.Fprintf(ctx, "Connection has been established at %s\n", ctx.ConnTime())
+// 	fmt.Fprintf(ctx, "Request has been started at %s\n", ctx.Time())
+// 	fmt.Fprintf(ctx, "Serial request number for the current connection is %d\n", ctx.ConnRequestNum())
+// 	fmt.Fprintf(ctx, "Your ip is %q\n\n", ctx.RemoteIP())
 
-	fmt.Fprintf(ctx, "Raw request is:\n---CUT---\n%s\n---CUT---", &ctx.Request)
+// 	fmt.Fprintf(ctx, "Raw request is:\n---CUT---\n%s\n---CUT---", &ctx.Request)
 
-	ctx.Response.Header.Add("X-Location", "google.com")
-	ctx.Response.SetStatusCode(fasthttp.StatusOK)
-}
+// 	ctx.Response.Header.Add("X-Location", "google.com")
+// 	ctx.Response.SetStatusCode(fasthttp.StatusOK)
+// }
 
 // 115     proxy_set_header X-Client-ID $client_id;
 // 116     proxy_set_header X-Client-URI $request_uri;
