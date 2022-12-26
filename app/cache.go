fe4eaf1ef84eecf11ffb1e00ac033687797bc0c9
Vadimka Komissarov <admin@vkom.cc>
2022-12-26 00:44:46 +0000

    cache - add zero value check

--- app/cache.go
+++ app/cache.go
@@ -1,13 +1,20 @@
 package app
 
-import "sync"
+import (
+	"errors"
+	"sync"
+)
 
 type CachedTitlesBucket struct {
 	locker sync.RWMutex
 	bucket map[uint32]*TitleSerie
 }
 
-func (m *CachedTitlesBucket) PullSerie(tid, sid uint16) (serie *TitleSerie) {
+func (m *CachedTitlesBucket) PullSerie(tid, sid uint16) (serie *TitleSerie, _ error) {
+	if tid == 0 || sid == 0 {
+		return serie, errors.New("cache: tid, sid zero found")
+	}
+
 	m.locker.RLock()
 
 	serie = m.bucket[uint32(tid)<<16|uint32(sid)]
@@ -16,9 +23,15 @@ func (m *CachedTitlesBucket) PullSerie(tid, sid uint16) (serie *TitleSerie) {
 	return
 }
 
-func (m *CachedTitlesBucket) PushSerie(serie *TitleSerie) {
+func (m *CachedTitlesBucket) PushSerie(serie *TitleSerie) (_ error) {
+	if serie == nil || serie.Title == 0 || serie.Serie == 0 {
+		return errors.New("cache: serie.Title, serie.Serie zero found or serie is undefined")
+	}
+
 	m.locker.Lock()
 
 	m.bucket[uint32(serie.Title)<<16|uint32(serie.Serie)] = serie
 	m.locker.Unlock()
+
+	return
 }
