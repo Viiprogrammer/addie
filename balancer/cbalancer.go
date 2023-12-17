6229ec85247e155acfa858f6048b1defa1f91857
Vadimka Komissarov <admin@vkom.cc>
2023-12-17 00:40:34 +0000

    disable debug

--- balancer/cbalancer.go
+++ balancer/cbalancer.go
@@ -185,9 +185,6 @@ func (m *ClusterBalancer) UpdateServers(servers map[string]net.IP) {
 	avail, _ := m.runtime.GetClusterA5bility()
 	m.ips, m.size = m.upstream.getIps(&m.ulock)
 
-	m.log.Warn().Msgf("calc - %d, avail - %d, dwn - %d, size - %d) cluster was marked as `NaN`",
-		dwn*100/m.size, 100-avail, dwn, m.size)
-
 	if (dwn != 0 && dwn*100/m.size > (100-avail)) || m.size == 0 {
 		m.isDown = true
 		m.log.Warn().Msgf("calc - %d, avail - %d, dwn - %d, size - %d) cluster was marked as `offline`",
