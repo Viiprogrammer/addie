ffd2ee5d5da27dda89368e79557d052fb1896764
Vadimka Komissarov <admin@vkom.cc>
2024-02-17 13:42:44 +0000

    swagger init

--- app/router.go
+++ app/router.go
@@ -22,8 +22,19 @@ import (
 	"github.com/rs/zerolog"
 
 	"github.com/gofiber/swagger"
+	_ "github.com/MindHunter86/addie/docs"
 )
 
+// @title Fiber Example API
+// @version 1.0
+// @description This is a sample swagger for Fiber
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email fiber@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8080
+// @BasePath /
 func (m *App) fiberConfigure() {
 	// time collector + logger
 	m.fb.Use(func(c *fiber.Ctx) (e error) {
