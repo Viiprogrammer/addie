a5264263f41aba8261b3e83b1cf22e8b58685cab
Vadimka Komissarov <admin@vkom.cc>
2024-03-05 15:03:28 +0000

    disable entry deploy stopping if duplicated consul-value recevied

--- runtime/entry.go
+++ runtime/entry.go
@@ -51,6 +51,11 @@ func (m *Entry) deploy(val interface{}) {
 		m.wg.Wait()
 	}
 
+	if val == m.candidate() {
+		log.Debug().Msg("requested value is already patching now, reject new deploy request")
+		return
+	}
+
 	if !m.deployed {
 		panic("unhandled conditions")
 	}
